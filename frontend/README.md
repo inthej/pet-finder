86171d94f3b0e21c533aa3c5a576455c3666b432
inthej <inthej12@gmail.com>
2022-08-13 16:59:09 +0900

    update: html layout & css

--- frontend/README.md
+++ frontend/README.md
@@ -9,3 +9,6 @@ $ npm init react-app .
 
 ## Reference
 - css position: [https://www.youtube.com/watch?v=0kA0mBvumrg](https://www.youtube.com/watch?v=0kA0mBvumrg)
+- css box-sizing: [https://opentutorials.org/course/2418/13405](https://opentutorials.org/course/2418/13405)
+- css inline block: [https://www.inflearn.com/blogs/376](https://www.inflearn.com/blogs/376)
+- css sample: [https://codepen.io/Loonz206/pen/gpRmja](https://codepen.io/Loonz206/pen/gpRmja)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,50 @@ import './styles/screen.css';
 function App() {
   return (
     <>
-      <header>
-        <span className="title">PetFinder</span>
-        <span className="clickable">메뉴01</span>
-        <span className="clickable">메뉴02</span>
-        <span className="clickable">메뉴03</span>
-      </header>
-      <div className="content">
-        <div className="window">
-          <div className="content">
-
-          </div>
-        </div>
-      </div>
+      {/* menu */}
+      <nav>
+        <ul>
+          <li>
+            <a href="http://localhost:3000/">Search</a>
+          </li>
+          <li>
+            <a href="http://localhost:3000/">Map</a>
+          </li>
+          <li>
+            <a href="http://localhost:3000/">Community</a>
+          </li>
+          <li>
+            <a href="http://localhost:3000/">Support</a>
+          </li>
+        </ul>
+      </nav>
+      {/* main article */}
+      <article>
+        <h1>펫을 찾아요</h1>
+        <picture>
+          <source
+            media="(min-width: 40em)"
+            srcSet="http://placehold.it/720x405 1x, http://placehold.it/1440x810 2x"
+          />
+          <source srcSet="http://placehold.it/405x228 1x, http://placehold.it/810x456 2x" />
+          <img src="http://placehold.it/705x405" alt="" />
+        </picture>
+        <p className="lead">
+          Maecenas sed diam eget risus varius blandit sit amet non magna. Integer
+          posuere erat a ante venenatis dapibus posuere velit aliquet.
+        </p>
+        <p>
+          Maecenas faucibus mollis interdum. Nullam id dolor id nibh ultricies
+          vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl
+          consectetur et.Cras justo odio, dapibus ac facilisis in, egestas eget
+          quam. Etiam porta sem malesuada magna mollis euismod. Curabitur blandit
+          tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus
+          dolor auctor. Cum sociis natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel
+          scelerisque nisl consectetur et. Nullam id dolor id nibh ultricies
+          vehicula ut id elit.
+        </p>
+      </article>
     </>
   );
 }
