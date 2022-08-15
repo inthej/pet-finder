a696c2e762bd752137238644ad03d665d7a93283
inthej <inthej12@gmail.com>
2022-08-15 14:21:30 +0900

    update menus and layouts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './styles/screen.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {
-  faDog,
+  faComment,
   faHandHoldingHeart,
+  faHouse,
   faLink,
-  faList,
   faLocationDot,
   faSearch,
   faTag
@@ -13,33 +13,30 @@ import {
 function App() {
   return (
     <>
-      <aside>
+      <div className="sidebar">
         <p>Pet Finder</p>
-        <a href="http://localhost:3000/">
-          <FontAwesomeIcon icon={faSearch} className="icon"/>
-          Search
-        </a>
-        <a href="http://localhost:3000/">
-          <FontAwesomeIcon icon={faLocationDot} className="icon"/>
-          Map
-        </a>
-        <a href="http://localhost:3000/">
-          <FontAwesomeIcon icon={faList} className="icon"/>
-          Community
-        </a>
-        <a href="http://localhost:3000/">
-          <FontAwesomeIcon icon={faHandHoldingHeart} className="icon"/>
-          Support
-        </a>
-        <a href="http://localhost:3000/">
-          <FontAwesomeIcon icon={faTag} className="icon"/>
-          Tags
-        </a>
-      </aside>
-      <div className="social">
-        <a href="http://localhost:3000/">
-          <FontAwesomeIcon icon={faLink} className="social-icon"/>
-        </a>
+        <ul>
+          <li><FontAwesomeIcon icon={faSearch} className="icon"/>검색</li>
+          <li><FontAwesomeIcon icon={faLocationDot} className="icon"/>지도</li>
+          <li><FontAwesomeIcon icon={faComment} className="icon"/>찾아주세요</li>
+          <li><FontAwesomeIcon icon={faHouse} className="icon"/>보호중입니다</li>
+          <li><FontAwesomeIcon icon={faHandHoldingHeart} className="icon"/>후원하기</li>
+          <li><FontAwesomeIcon icon={faTag} className="icon"/>태그</li>
+        </ul>
+
+        <div className="footer">
+          로그인 | 회원가입
+          {/*내정보 | 로그아웃*/}
+        </div>
+      </div>
+
+      <div className="toplink">
+        <FontAwesomeIcon icon={faLink} className="icon"/>
+      </div>
+
+      <div className="content">
+        <h2>펫을 찾아요</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus aliquid aspernatur debitis doloribus fugit id illum quaerat quod tempore veniam? Earum error id illo ipsum non perspiciatis sapiente sit tempora.</p>
       </div>
     </>
   );
